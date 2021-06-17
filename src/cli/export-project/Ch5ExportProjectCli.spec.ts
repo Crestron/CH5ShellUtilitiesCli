33f4265a60e575ae8506119955a799d83ea60962
Calin Pirosca <calin.pirosca@corebuild.eu>
2021-06-17 08:45:21 +0300

    chore(export-project): rename stubs / spies

--- src/cli/export-project/Ch5ExportProjectCli.spec.ts
+++ src/cli/export-project/Ch5ExportProjectCli.spec.ts
@@ -14,15 +14,15 @@ describe('Export a project >>>>>>>> ', () => {
 
     it(`Expect export project to return true`, async () => {
         // Mock functions which gets called inside exportProjectComponent.run so they are not actually called
-        const deleteFilespy = sinon.stub(Ch5CliUtil.prototype, 'deleteFile');
+        const deleteFileStub = sinon.stub(Ch5CliUtil.prototype, 'deleteFile');
         const copyFilesSpy = sinon.spy(exportProjectComponent, 'copyFiles');
-        const createTempFolder = sinon.spy(exportProjectComponent, 'createTempFolder');
+        const createTempFolderSpy = sinon.spy(exportProjectComponent, 'createTempFolder');
 
         const response = await exportProjectComponent.run();
-        expect(deleteFilespy.called).equals(true);
-        expect(deleteFilespy.calledWith('./dist/@crestron/ch5-shell-utilities-cli.zip')).equals(true);
-        expect(createTempFolder.called).equals(true);
-        expect(createTempFolder.calledWith('@crestron/ch5-shell-utilities-cli-Code-Folder-Temp')).equals(true);
+        expect(deleteFileStub.called).equals(true);
+        expect(deleteFileStub.calledWith('./dist/@crestron/ch5-shell-utilities-cli.zip')).equals(true);
+        expect(createTempFolderSpy.called).equals(true);
+        expect(createTempFolderSpy.calledWith('@crestron/ch5-shell-utilities-cli-Code-Folder-Temp')).equals(true);
 
         expect(copyFilesSpy.called).equals(true);
 
