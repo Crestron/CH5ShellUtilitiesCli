29088b340f084fd20c9e7ce7bbb78dc2e300a40f
Calin Pirosca <calin.pirosca@corebuild.eu>
2021-06-17 11:47:08 +0300

    chore(export-project): update tests for export project

--- src/cli/export-project/Ch5ExportProjectCli.spec.ts
+++ src/cli/export-project/Ch5ExportProjectCli.spec.ts
@@ -1,31 +1,64 @@
-import { expect } from 'chai';
+import {expect} from 'chai';
 import * as sinon from 'sinon';
-
+import {SinonSpy, SinonStub} from 'sinon';
+import mock from 'mock-fs';
 import {Ch5ExportProjectCli} from "./Ch5ExportProjectCli";
 import {Ch5CliUtil} from "../Ch5CliUtil";
+import {Ch5CliNamingHelper} from "../Ch5CliNamingHelper";
+
+const fs = require('fs');
+const fsExtra = require('fs-extra');
 
 const exportProjectComponent = new Ch5ExportProjectCli();
 
 describe('Export a project >>>>>>>> ', () => {
-    afterEach(() => {
-        // Revert any stubs / mocks created using sinon
-        sinon.restore();
+  let deleteFileStub: SinonStub;
+  let copyFilesSpy: SinonSpy;
+  let createTempFolderSpy: SinonSpy;
+  let removeAllSpacesSpy: SinonSpy;
+  before(() => {
+    // Mock the fs and provide a mocked package.json
+    mock({
+      'package.json': '{"name":"@crestron/ch5-shell-utilities-cli"}'
     });
+  });
 
-    it(`Expect export project to return true`, async () => {
-        // Mock functions which gets called inside exportProjectComponent.run so they are not actually called
-        const deleteFileStub = sinon.stub(Ch5CliUtil.prototype, 'deleteFile');
-        const copyFilesSpy = sinon.spy(exportProjectComponent, 'copyFiles');
-        const createTempFolderSpy = sinon.spy(exportProjectComponent, 'createTempFolder');
 
-        const response = await exportProjectComponent.run();
-        expect(deleteFileStub.called).equals(true);
-        expect(deleteFileStub.calledWith('./dist/@crestron/ch5-shell-utilities-cli.zip')).equals(true);
-        expect(createTempFolderSpy.called).equals(true);
-        expect(createTempFolderSpy.calledWith('@crestron/ch5-shell-utilities-cli-Code-Folder-Temp')).equals(true);
+  after(() => {
+    mock.restore();
+  });
 
-        expect(copyFilesSpy.called).equals(true);
+  beforeEach(() => {
+    // Mock functions which gets called inside exportProjectComponent.run so they are not actually called
+    deleteFileStub = sinon.stub(Ch5CliUtil.prototype, 'deleteFile');
+    removeAllSpacesSpy = sinon.spy(Ch5CliNamingHelper.prototype, 'removeAllSpaces');
+    copyFilesSpy = sinon.spy(exportProjectComponent, 'copyFiles');
+    createTempFolderSpy = sinon.spy(exportProjectComponent, 'createTempFolder');
+  });
 
-        expect(response).equals(true);
-    });
+  afterEach(() => {
+    // Revert any stubs / mocks created using sinon
+    sinon.restore();
+  });
+
+  it(`Expect export project to return true`, async () => {
+    const response = await exportProjectComponent.run();
+    expect(response).equals(true);
+  });
+
+  it('Expect file names to equal project name', async () => {
+    const readJSONSyncSpy = sinon.spy(fsExtra, 'readJSONSync');
+
+    await exportProjectComponent.run();
+
+    expect(readJSONSyncSpy.called).equals(true);
+    expect(removeAllSpacesSpy.called).equals(true);
+    expect(removeAllSpacesSpy.calledWith('@crestron/ch5-shell-utilities-cli'));
+    expect(createTempFolderSpy.calledWith('@crestron/ch5-shell-utilities-cli-Code-Folder-Temp'));
+  });
+
+  it('Expect functions to be called in certain order', async () => {
+    await exportProjectComponent.run();
+    sinon.assert.callOrder(removeAllSpacesSpy, deleteFileStub, createTempFolderSpy, copyFilesSpy);
+  })
 });
